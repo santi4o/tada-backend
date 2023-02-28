a1f3adb5b6457364e1c66da1d99b4e18026dfad3
Aldo Santiago <aldo11santiago@gmail.com>
2023-02-28 13:56:15 -0600

    fix: changed record to class for Task

--- src/main/java/com/encora/todos/entities/Task.java
+++ src/main/java/com/encora/todos/entities/Task.java
@@ -1,12 +1,91 @@
 package com.encora.todos.entities;
 
-public record Task(Integer id, String name) {
-    public Task(Integer id, String name) {
-        this.id = id;
-        this.name = name;
+import java.util.Date;
+
+public class Task {
+    private static int count = 0;
+    private Integer id;
+    private String text;
+    private Integer priority;
+    private Date creationDate;
+    private Date dueDate;
+    private Boolean done;
+    private Date doneDate;
+
+    public Task() {}
+
+    public Task(String text, Integer priority, Date creationDate, Date dueDate, Boolean done) {
+        this.text = text;
+        setPriority(priority);
+        this.creationDate = creationDate;
+        this.dueDate = dueDate;
+        this.done = done;
+        setId(++count);
     }
 
-    public Task(String name) {
-        this(null, name);
+    public Integer getId() {
+        return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        if (priority >= 0 && priority <= 2) {
+            this.priority = priority;
+        } else {
+            throw new IllegalArgumentException("priority out of range (0-2)");
+        }
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public Date getDoneDate() {
+        return doneDate;
+    }
+
+    public void setDoneDate(Date doneDate) {
+        this.doneDate = doneDate;
+    }
+
+    
+
+    
+
+
 }
--- src/test/java/com/encora/todos/entities/TaskTest.java
+++ src/test/java/com/encora/todos/entities/TaskTest.java
@@ -1,14 +1,32 @@
 package com.encora.todos.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
     @Test
-    public void createTask() {
-        Task task = new Task("Todo test");
-        assertEquals(task.id(), null);
-        assertEquals(task.name(), "Todo test");
+    public void createTasks() {
+        Date now = new Date();
+        Task task = new Task("learn react", 2, now, null, false);
+        assertEquals(task.getId(), 1);
+        assertEquals(task.getText(), "learn react");
+        Task task2 = new Task("keep learning", 0, now, null, false);
+        assertEquals(task2.getId(), 2);
+        assertEquals(task2.getText(), "keep learning");
+    }
+
+    @Test
+    public void invalidPriority() {
+        Date now = new Date();
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("learn react", 3, now, null, false);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Task("learn react", -1, now, null, false);
+        });
     }
 }
