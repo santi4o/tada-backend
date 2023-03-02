f25ddee7851de8240539dd52872f3d91efbdf667
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-01 23:56:04 -0600

    feat: added hashcode, equals & toString m to Task

--- src/main/java/com/encora/todos/entities/Task.java
+++ src/main/java/com/encora/todos/entities/Task.java
@@ -83,4 +83,73 @@ public class Task {
         this.doneDate = doneDate;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        result = prime * result + ((priority == null) ? 0 : priority.hashCode());
+        result = prime * result + ((creationDate == null) ? 0 : creationDate.hashCode());
+        result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
+        result = prime * result + ((done == null) ? 0 : done.hashCode());
+        result = prime * result + ((doneDate == null) ? 0 : doneDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (text == null) {
+            if (other.text != null)
+                return false;
+        } else if (!text.equals(other.text))
+            return false;
+        if (priority == null) {
+            if (other.priority != null)
+                return false;
+        } else if (!priority.equals(other.priority))
+            return false;
+        if (creationDate == null) {
+            if (other.creationDate != null)
+                return false;
+        } else if (!creationDate.equals(other.creationDate))
+            return false;
+        if (dueDate == null) {
+            if (other.dueDate != null)
+                return false;
+        } else if (!dueDate.equals(other.dueDate))
+            return false;
+        if (done == null) {
+            if (other.done != null)
+                return false;
+        } else if (!done.equals(other.done))
+            return false;
+        if (doneDate == null) {
+            if (other.doneDate != null)
+                return false;
+        } else if (!doneDate.equals(other.doneDate))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [id=" + id + ", text=" + text + ", priority=" + priority + ", creationDate=" + creationDate
+                + ", dueDate=" + dueDate + ", done=" + done + ", doneDate=" + doneDate + "]";
+    }
+
+    
+
 }
